f3e872510deb6bb841bc09f29d658f6193aeee5e
default_git_name <default_git_email>
2014-11-21 11:24:33 +0000

    next attempt at hello world. Still not there.

--- creations/hello-world/on-create.scm
+++ creations/hello-world/on-create.scm
@@ -1,13 +1,16 @@
-(require 'android-defs)
+(begin
 
-(define (logi . messages)
- (android.util.Log:i "on-create.scm" (apply string-append messages)))
- 
-(logi "Starting")
-
-((current-activity):setContentView
- (android.widget.TextView (current-activity)
-   text: "Hello World"))
-
-(logi "Ending")
+  (require 'android-defs)
   
+  (define (logi . messages)
+   (android.util.Log:i "on-create.scm" (apply string-append messages)))
+   
+  (logi "Starting")
+  
+  (let ((t (android.widget.TextView (current-activity))))
+   (t:set-text "Hello World"))
+   
+  (<android.app.Activity>:setContentView
+   (current-activity) (as android.view.View t))
+  
+  (logi "Ending"))
